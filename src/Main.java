6fbeaae10f2f0a17a2ba796156417a1f24343c3f
Vincent Salazar <100139821@lubbockisd.net>
2022-09-13 09:47:17 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+public class Variables
+{
+    public static void main ( String[] args )
+    {
+        //define 1 variable of each of the
+        //following data types
+        //byte	short		int 		long
+        //float	double
+        //char      boolean	String
+
+        //integer variables
+        byte byteOne = 127;
+        short shortOne = 32767;
+        int intOne = 2147493647;
+        long longOne = //9quintillion;
+        float floatOne = 39.627F;
+        double doubleOne = 847956.736485903;
+
+        System.out.println("/////////////////////////////////");
+        System.out.println("*Vincent Salazar          09/13/22*");
+        System.out.println("*                               *");
+        System.out.println("*        integer types          *");
+        System.out.println("*                               *");
+        System.out.println("*8 bit - byteOne = "+byteOne+"\t\t*");
+        System.out.println("*16 bit - shortOne = "+shortOne+"\t\t*");
+        System.out.println("*32 bit - intOne = "+intOne+"\t\t*");
+        System.out.println("*64 bit - longOne = "+longOne+"\t\t*");
+        System.out.println("*32 bit - floatOne = "+floatOne+"\t\t*");
+        System.out.println("*64 bit - doubleOne = "+doubleOne+"\t\t*");
+    }
+}
